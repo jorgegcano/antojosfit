33e899df3e306e0bbba3e6bd1c5160ed932e0d31
jorgegcano <garciacano.jrg@gmail.com>
2021-02-04 12:36:38 +0100

    grid con flexbox en la home

--- themes/antojosFit/front-page.php
+++ themes/antojosFit/front-page.php
@@ -9,7 +9,7 @@
     <div class="contenedor seccion">
         <!--<h2 class="text-center texto-primario">Nuestros Productos</h2>-->
         <img class="logo-blog" src=<?php echo get_template_directory_uri()."/img/anto-jos-fit-TYPO.png" ?> width=200 alt="logo">
-        <?php antojosfit_lista_productos(4); ?>
+        <?php antojosfit_lista_productos(6); ?>
         <div class="contenedor-boton">
             <a class="boton boton-primario" href="<?php echo esc_url(get_permalink(get_page_by_title('shop'))); ?>">Ver Carta Completa</a>
         </div>
--- themes/antojosFit/inc/queries.php
+++ themes/antojosFit/inc/queries.php
@@ -2,26 +2,39 @@
 
 function antojosfit_lista_productos($cantidad = -1) { ?>
 
-    <ul class=" <?php echo 'listado-productos'; ?>">
         <?php
             $args = array(
                 'post_type'=>'product',
                 'posts_per_page' => $cantidad,
             );
             $productos = new WP_Query($args);
+            $ul = 0;
+            $li = 0;
             while($productos->have_posts()) : $productos->the_post();
+            
+            if ($li %2 == 0) {
+                if ($ul %2 == 0) {
+                    echo "<ul class='fila-par'>";
+                } else {
+                    echo "<ul class='fila-impar'>";
+                }
+                $ul++;
+            }            
         ?>
 
             <li class="producto front-page">
-            <?php do_action( 'woocommerce_before_shop_loop_item' ); ?>
+            <a href="<?php the_permalink() ?>">
             <?php the_post_thumbnail('mediano'); ?>
             <div class="contenido text-center">
                 <h3><?php the_title(); ?></h3>
             </div>
+            </a>
             </li>
-
-
-            <?php endwhile; wp_reset_postdata(); ?>
-    </ul>
+            <?php if ($li %2 != 0) { 
+                echo "</ul>";
+                } 
+                $li++; ?>
+        
+            <?php  endwhile; wp_reset_postdata(); ?>
 <?php
 }
